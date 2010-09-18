f2a8200ff312f109dfec8789c332e1b4e187f7c8
marcosvpcortes <o.planeta@gmail.com>
2010-09-18 15:21:03 -0300

    Revert "Primeiro commit do CLI / Workspace"
    This reverts commit bcbda1d9a59daeefb913e1af7207abe398fff5ae.

--- src/main/java/br/uff/ic/gardener/App.java
+++ src/main/java/br/uff/ic/gardener/App.java
@@ -1,20 +1,13 @@
 package br.uff.ic.gardener;
 
-import br.uff.ic.gardener.cli.CLI;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-	/**
-	 * call the CLI interface
-	 * @param args the parameters of aplication
-	 */
     public static void main( String[] args )
     {
-    	CLI.doMain(args);
-        //System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
