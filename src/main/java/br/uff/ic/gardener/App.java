d0bf01c0f3d52c35df8f59ec605a11be2d0a253f
André Coelho <andre.ribeiro.coelho@gmail.com>
2010-10-02 03:15:52 -0300

    Just Formating

--- src/main/java/br/uff/ic/gardener/App.java
+++ src/main/java/br/uff/ic/gardener/App.java
@@ -2,12 +2,10 @@ package br.uff.ic.gardener;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
