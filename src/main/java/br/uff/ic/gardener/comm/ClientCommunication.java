d21ef2d9ddae96793aa719d1d3285b62855da8cc
cslaviero <cslaviero@gmail.com>
2010-10-27 22:42:28 -0200

    Alterações feitas na GUI  e novos testes com API Comunicação.

--- src/main/java/br/uff/ic/gardener/comm/ClientCommunication.java
+++ src/main/java/br/uff/ic/gardener/comm/ClientCommunication.java
@@ -6,17 +6,21 @@ package br.uff.ic.gardener.comm;
 
 import br.uff.ic.gardener.RevisionID;
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.*;
 
 /**
  *
@@ -24,30 +28,39 @@ import java.util.logging.Logger;
  */
 public class ClientCommunication {
 
+    Socket mySocket;
+
     public ClientCommunication() {
     }
 
-     Map<String,OutputStream> doCheckout(String serverURL, RevisionID revision) {
+    Map<String, OutputStream> doCheckout(String serverURL, RevisionID revision) {
 
         Object checkOut = null;
-        Socket mySocket = null;
+        mySocket = null;
         BufferedWriter bw = null;
         try {
-            mySocket = new Socket(serverURL, 20102);
+            mySocket = new Socket(serverURL, 50000);
 
+            System.out.println("Connected");
             PrintStream ps = new PrintStream(mySocket.getOutputStream());
             ps.println("Estou enviando dados para o servidor");
 
+            //peguei o nome do arquivo e vou salvar no diretório do working space
             File myFile = new File("c:\\text.txt");
+            //crie um novo byte array do tamanho do arquivo
             byte[] mybytearray = new byte[(int) myFile.length()];
+            //crie o fileinputstream
             FileInputStream fis = new FileInputStream(myFile);
+            //crie um buffered inputstream a partir do fileinputstream
             BufferedInputStream bis = new BufferedInputStream(fis);
+            //leia PARA o bytearray
             bis.read(mybytearray);
             OutputStream os = mySocket.getOutputStream();
             System.out.println("Sending...");
+            //envia o bytearray
             os.write(mybytearray);
+            // flush para dar "vazão"
             os.flush();
-
 
         } catch (UnknownHostException ex) {
             Logger.getLogger(ClientCommunication.class.getName()).log(Level.SEVERE, null, ex);
@@ -64,7 +77,21 @@ public class ClientCommunication {
         return null;
     }
 
-    public RevisionID doCheckin(String serverURL, Map<String,OutputStream> items, String user, String message){
+    public String receiveMessage() throws IOException {
+        InputStreamReader isr = new InputStreamReader(mySocket.getInputStream());
+
+       BufferedReader br = new BufferedReader(isr);
+
+        return br.readLine();
+    }
+
+    public void sendMessage(String message) throws IOException {
+        PrintWriter pw = new PrintWriter(mySocket.getOutputStream());
+
+        pw.print(message);
+    }
+
+    public RevisionID doCheckin(String serverURL, Map<String, OutputStream> items, String user, String message) {
         return null;
     }
 
@@ -72,7 +99,8 @@ public class ClientCommunication {
 
         ClientCommunication comm = new ClientCommunication();
 
-       comm.doCheckout("192.168.0.101",new RevisionID(0));
+        comm.doCheckout("187.115.251.106", new RevisionID(0));
+       // comm.doCheckout("192.168.0.101", new RevisionID(0));
 
     }
 }
