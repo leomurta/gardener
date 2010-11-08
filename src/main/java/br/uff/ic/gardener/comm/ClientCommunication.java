67fb99107e500ade2c1b830111ddeadec5b6b33c
Cleyton Slaviero <cslaviero@gmail.com>
2010-11-08 13:09:34 -0200

    Criação de um protocolo simples de comunicação entre ClientCommunication.java e ServerListener.java.
    De início, temos o método doCheckin , em ClientCommunication.java que se comunica com o ServerListener.java. Falta implementar o envio dos itens de configuração (aguardando decisão sobre a estrutura da classe ConfigurationItem.java).

--- src/main/java/br/uff/ic/gardener/comm/ClientCommunication.java
+++ src/main/java/br/uff/ic/gardener/comm/ClientCommunication.java
@@ -4,22 +4,19 @@
  */
 package br.uff.ic.gardener.comm;
 
+import br.uff.ic.gardener.ConfigurationItem;
 import br.uff.ic.gardener.RevisionID;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -28,79 +25,146 @@ import java.util.logging.Logger;
 public class ClientCommunication {
 
     Socket mySocket;
+    InputStream in;
+    OutputStream out;
 
-    public ClientCommunication() {
+    public ClientCommunication(String serverURL) throws IOException {
+
+        mySocket = new Socket(serverURL, 50000);
+
+        in = mySocket.getInputStream();
+        out = mySocket.getOutputStream();
+
+        System.out.println("Connected");
+
     }
 
-    Map<String, OutputStream> doCheckout(String serverURL, RevisionID revision) {
+    public Map<String, OutputStream> doCheckout(String project, RevisionID revision) throws IOException {
 
-        Object checkOut = null;
-        mySocket = null;
-        BufferedWriter bw = null;
-        try {
-            mySocket = new Socket(serverURL, 50000);
-            
-            System.out.println("Connected");
-            PrintStream ps = new PrintStream(mySocket.getOutputStream());
-            ps.println("Estou enviando dados para o servidor");
 
-            //peguei o nome do arquivo e vou salvar no diretório do working space
-            File myFile = new File("c:\\text.txt");
-            //crie um novo byte array do tamanho do arquivo
-            byte[] mybytearray = new byte[(int) myFile.length()];
-            //crie o fileinputstream
-            FileInputStream fis = new FileInputStream(myFile);
-            //crie um buffered inputstream a partir do fileinputstream
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            //leia PARA o bytearray
-            bis.read(mybytearray);
-            OutputStream os = mySocket.getOutputStream();
-            System.out.println("Sending...");
+        // 1 - envia mensagem ao servidor solicitando o checkout
+        sendMessage("CO");
 
-            //envia o bytearray
-            os.write(mybytearray);
-            // flush para dar "vazão"
-            os.flush();
+        String ack = receiveMessage();
 
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(ClientCommunication.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ClientCommunication.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                mySocket.close();
-            } catch (IOException ex) {
-                Logger.getLogger(ClientCommunication.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        //envio o projeto de onde eu quero os itens
+
+        sendMessage(project);
+
+        ack = receiveMessage();
+
+        //manda qual a revisão eu quero
+        sendMessage(project);
+
+        ack = receiveMessage();
+
+        // recebe os arquivos
+        //TODO Criar método para receber os arquivos
+
+        // organiza os arquivos e retorna
+        return null;
+
+
+    }
+
+    private String receiveMessage() throws IOException {
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+        String linha = br.readLine();
+
+        return linha;
+    }
+
+    private void sendMessage(String message) throws IOException {
+        System.out.println("sending message" + message);
+        //  PrintWriter pw = new PrintWriter(out, true);
+        PrintWriter pw = new PrintWriter(out, true);
+        pw.println(message);
+
+    }
+    /**
+     * Not working.
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private Object receiveObject() throws IOException, ClassNotFoundException {
+
+        Object obj = null;
+
+        ObjectInputStream objInp = new ObjectInputStream(in);
+
+        obj = objInp.readObject();
+
+        return obj;
+    }
+
+    /**
+     * Not working.
+     * @param obj
+     * @throws IOException
+     */
+    private void sendObject(Object obj) throws IOException {
+        ObjectOutputStream objOut = new ObjectOutputStream(out);
+
+        objOut.writeObject(obj);
+    }
+
+    /**
+     * Creates a new revison on the server specified by the URL
+     * @param serverURL
+     * @param items
+     * @param user
+     * @param message
+     * @return
+     * @throws MalformedURLException
+     */
+    public RevisionID doCheckin(Map<String, OutputStream> items, String user, String message) throws MalformedURLException, IOException {
+
+        String project = "default";
+        // 1 - enviar a solicitação de checkin e aguardar
+
+        sendMessage("CI");
+        String ack = receiveMessage();
+
+        // 2 - depois da resposta, enviar nome do projeto (não há como pegar isso, ainda)
+        sendMessage(project);
+
+        ack = receiveMessage();
+
+        // 3 - enviar o usuário
+        sendMessage(user);
+
+        ack = receiveMessage();
+
+        // 4 - enviar a msg
+        sendMessage(message);
+
+        ack = receiveMessage();
+
+        // 5 - depois da confirmação enviar os arquivos
+        //TODO send ConfigurationItems via ObjectInputStream
+
+        // por fim, receber o número da revisão
+        String worked = receiveMessage();
+
+        System.out.println("I hope " + worked);
 
         return null;
     }
 
-    public String receiveMessage() throws IOException {
-        InputStreamReader isr = new InputStreamReader(mySocket.getInputStream());
+    public void doInit(String serverURL) {
+        //TODO implement!
 
-       BufferedReader br = new BufferedReader(isr);
-
-        return br.readLine();
     }
 
-    public void sendMessage(String message) throws IOException {
-        PrintWriter pw = new PrintWriter(mySocket.getOutputStream());
+    public static void main(String[] args) throws IOException {
 
-        pw.print(message);
-    }
+        ClientCommunication comm = new ClientCommunication("localhost");
 
-    public RevisionID doCheckin(String serverURL, Map<String, OutputStream> items, String user, String message) {
-        return null;
-    }
-
-    public static void main(String[] args) {
-
-        ClientCommunication comm = new ClientCommunication();
-
-        comm.doCheckout("172.16.0.240", new RevisionID(0));
-       // comm.doCheckout("192.168.0.101", new RevisionID(0));
+        //  comm.doCheckout("172.16.0.240", new RevisionID(0));
+        // comm.doCheckout("192.168.0.101", new RevisionID(0));
+        comm.doCheckin(null, "cleyton", "I'm working!");
 
     }
 }
--- src/main/java/br/uff/ic/gardener/comm/ServerListener.java
+++ src/main/java/br/uff/ic/gardener/comm/ServerListener.java
@@ -4,7 +4,7 @@
  */
 package br.uff.ic.gardener.comm;
 
-import br.uff.ic.gardener.server.Project;
+//import br.uff.ic.gardener.server.Project;
 import br.uff.ic.gardener.server.Server;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -13,6 +13,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -31,6 +34,8 @@ public class ServerListener implements Runnable {
     ServerSocket serverSocket;
     int filesize = 6022386; // filesize temporarily hardcoded
     Socket mySocket;
+    InputStream in;
+    OutputStream out;
 
     public ServerListener() {
     }
@@ -132,26 +137,62 @@ public class ServerListener implements Runnable {
         }
     }
 
-    public String receiveMessage() throws IOException {
+    private String receiveMessage() throws IOException {
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+        String linha = br.readLine();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
-
-        return br.readLine();
+        return linha;
     }
 
-    public void sendMessage(String message) throws IOException {
-        PrintWriter pw = new PrintWriter(mySocket.getOutputStream());
+    private void sendMessage(String message) throws IOException {
+        // System.out.println("sending message" + message);
+        //  PrintWriter pw = new PrintWriter(out, true);
+        PrintWriter pw = new PrintWriter(out, true);
+        pw.println(message);
 
-        pw.print(message);
+    }
+    /**
+     * Comando para receber um objeto (não funciona)
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private Object receiveObject() throws IOException, ClassNotFoundException {
+
+        Object obj = null;
+
+        ObjectInputStream objInp = new ObjectInputStream(in);
+
+        obj = objInp.readObject();
+
+        return obj;
+    }
+    /**
+     * Comando para enviar objeto (não funciona)
+     * @param obj
+     * @throws IOException
+     */
+    private void sendObject(Object obj) throws IOException {
+        ObjectOutputStream objOut = new ObjectOutputStream(out);
+
+        objOut.writeObject(obj);
     }
 
+    /**
+     * verifca a intenção e realiza a operação de acordo
+     * @param intent
+     * @throws IOException
+     */
     private void doOperation(String intent) throws IOException {
         if (intent.compareTo("CO") == 0) {
             //faça o procedimento de checkout
             sendCheckout();
         } else {
             if (intent.compareTo("CI") == 0) {
-                receiveCheckin();
+                String message = receiveCheckin();
+                sendMessage(message);
+
             } else {
                 if (intent.compareTo("LS") == 0) {
                     sendList();
@@ -175,17 +216,23 @@ public class ServerListener implements Runnable {
             // 2- aguardar uma conexão
             mySocket = serverSocket.accept();
             //2.1 - depois de aberto, imprimir o endereço da conexão originária
-            System.out.println("InetAdress: " + mySocket.getInetAddress() + " porta: " + mySocket.getPort());
+            System.out.println("Conectado a InetAdress: " + mySocket.getInetAddress() + " porta: " + mySocket.getPort());
+
+            in = mySocket.getInputStream();
+            out = mySocket.getOutputStream();
 
             /*
              * Pelo procotolo, vamos esperar uma mensagem dizendo o que é que vai ser feito:
              *  CO, CI ou LS
              */
-            //3 - pegando o inputstream para escutar o que vai ser enviado
+            //3 - pegando a intenção
             String intent = receiveMessage();
 
             //4 - de acordo com cada opção, uma operação vai ser executada
             doOperation(intent);
+            //nota: por enquanto, elefaz uma operação e para, mas podemos
+            //colocar a execução num looping para esperar a próxima solicitação.
+
 
         } catch (IOException ex) {
             Logger.getLogger(ServerListener.class.getName()).log(Level.SEVERE, null, ex);
@@ -197,7 +244,6 @@ public class ServerListener implements Runnable {
                 Logger.getLogger(ServerListener.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
     }
 
     /**
@@ -210,7 +256,7 @@ public class ServerListener implements Runnable {
 
         String projectName = null;
         String revisionNumber = null;
-        Server servidor = new Server();
+        Server servidor = Server.getInstance();
 
         //1 - servidor precisa dar um "ack" para continuar o processo de envio de informações
         sendMessage("ack");
@@ -222,14 +268,15 @@ public class ServerListener implements Runnable {
         //TODO fazer um método que já jogue essas informações em um arquivo de projeto...
         // aqui, apenas estamos recebendo um arquivo qualquer. Talvez essa não seja a melhor forma,
         // e talvez tenhamos que pegar informação por informação (nome do projeto e revisão)
-        File configFile = receiveFile();
+        // File configFile = receiveFile();
 
-        Project project = new Project(projectName);
-        ArrayList metadataArray = servidor.ckeckout(null, project, revisionNumber);
+        // Project project = new Project(projectName);
+        // String project  = "";
+        //ArrayList metadataArray = servidor.ckeckout(null, project, revisionNumber);
 
         //assim que receber os dados de versão do servidor, preciso pegar os arquivos associados àquela revisão.
 
-        File[] files = servidor.ckeckoutFile(project, revisionNumber);
+        //  File[] files = servidor.ckeckoutFile(project, revisionNumber);
 
         //3 - após pegar os arquivos, enviá-los ao cliente que solicitou. ele
         // será responsável por tratar os arquivos
@@ -237,8 +284,44 @@ public class ServerListener implements Runnable {
 
     }
 
-    private void receiveCheckin() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private String receiveCheckin() throws IOException {
+        String project = null;
+        String user = null;
+        String date = null;
+        String message = null;
+        ArrayList itens = null;
+        String result = " it worked!";
+        // neste momento, recebi o intent de checkout. O cliente está esperando um ok 
+        sendMessage("ack");
+
+        // 1 - Depois do ok, precisamos receber as informações.
+        // Na sequência, receberemos o projeto, usuário, msg, e os arquivos.
+
+        // recebendo o projeto
+        project = receiveMessage();
+        //manipula o resultado
+        sendMessage("ack");
+
+        // recebendo o usuário
+        user = receiveMessage();
+        //manipula o resultado
+        sendMessage("ack");
+
+        message = receiveMessage();
+        //manipula o resultado
+        sendMessage("ack");
+
+        //TODO receber arquivo (s)
+
+        System.out.println("Hi, i'm " + user + " and i'm trying to reach " + project + " project with this message: " + message);
+
+        //TODO armazenar arquivos numa ArrayList
+
+        //ConfigurationItem configItem = new ConfigurationItem(uri, InputStream, CIType.file, revision, user);
+
+        Server server = Server.getInstance();
+        // result = server.ckeckIn(project, user, date, message, "", itens);
+        return result;
     }
 
     private void sendList() {
@@ -246,7 +329,9 @@ public class ServerListener implements Runnable {
     }
 
     /**
-     * Esse método é responsável por receber um arquivo
+     * Esse método é responsável por receber um arquivo. Não está pronto,
+     * mas é o mais importantes na comunicação. Quando pronto, seremos capazes de enviar e receber arquivos
+     * pela rede, ao invés de apenas trocar mensagens
      * @return
      */
     private File receiveFile() throws IOException {
@@ -279,6 +364,7 @@ public class ServerListener implements Runnable {
 
         //escreva buffered output stream
         //TODO o nome do arquivo precisa ser informado de outra forma.
+        // na verdade, precisamos só armazenar o array de bytes em algum lugar.
         FileOutputStream fos = new FileOutputStream("C:\\Users\\Cleyton\\Desktop\\file.txt");
         BufferedOutputStream bos = new BufferedOutputStream(fos);
         bos.write(mybytearray, 0, current);
@@ -294,4 +380,9 @@ public class ServerListener implements Runnable {
 //        ServerListener sl = new ServerListener();
 //        sl.listen();
 //    }
+
+    public static void main(String[] args) {
+        ServerListener server = new ServerListener();
+        server.run();
+    }
 }
