5ad68941e95c3229483b8494a73fd6ae2a30bb06
ffloriano <ffloriano@ic.uff.br>
2010-11-11 09:00:28 -0200

    UnifiedFormat / ContextFormat / LessContextFormat

--- src/main/java/br/uff/ic/gardener/diff/LessContextFormat.java
+++ src/main/java/br/uff/ic/gardener/diff/LessContextFormat.java
@@ -1,42 +1,176 @@
 package br.uff.ic.gardener.diff;
-/* -------------- DETAILS DESCRIPTION OF LESS CONTEXT FORMAT --------------
- *  ***************
- *  *** from-file-line-numbers ****
- * from-file-line
- * from-file-line...
- * --- to-file-line-numbers ----
- * to-file-line
- * to-file-line...
- *
- * ‘!’ A line that is part of a group of one or more lines that changed between the two files. There is a corresponding group of lines marked with ‘!’ in the part of this hunk for the other file.
- * ‘+’ An "inserted" line in the second file that corresponds to nothing in the first file.
- * ‘-’ A "deleted" line in the first file that corresponds to nothing in the second file.
- * -------------------------------------------------------------------
- */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LessContextFormat implements IFormat {
 
+
     @Override
     public void format(IResultDiff resultDiff) {
-        System.out.println("Less Context Format");
+        int sizeContext = 0;
+        LinesBean line = null;
+        List linesOutputF1 = new ArrayList();
+        List linesOutputF2 = new ArrayList();
+
+        boolean hasLineDeletedOrAdded = false;
+        int idInitialLine1 = -1, idInitialLine2 = -1, idFinalLine1 = -1, idFinalLine2 = -1;
+        boolean isNext = false;
+        boolean hasLine = false;
+
+        PrintWriter outputWriter = null;
+        try {
+            outputWriter = WriterFactory.getWriter();
+        } catch (IOException ex) {
+            Logger.getLogger(NormalFormat.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        outputWriter.println(FormatFactory.getMainHeader().split("\\!!")[0]);
+        outputWriter.println(FormatFactory.getMainHeader().split("\\!!")[1]);
+        outputWriter.println();
+
+        for (Iterator it = resultDiff.getResult().iterator(); it.hasNext();) {
+            hasLine = false;
+            if (!isNext) {
+                line = (LinesBean) it.next();
+            }
+            do {
+                if (line.getSituation() == LinesBean.Situation.ADDED) {
+                    if (sizeContext > 1) {
+                        linesOutputF2.remove(0);
+                        sizeContext--;
+                    }
+                    hasLineDeletedOrAdded = true;
+                    linesOutputF2.add("+ " + line.getLine());
+                    sizeContext = 0;
+                    if (idInitialLine2 == -1) {
+                        idInitialLine2 = line.getIndLineFileV2() + 1;
+                    }
+                    idFinalLine2 = line.getIndLineFileV2() + 1;
+                } else {
+                    if (line.getSituation() == LinesBean.Situation.REMOVED) {
+                        if (sizeContext > 1) {
+                            linesOutputF1.remove(0);
+                            sizeContext--;
+                            idInitialLine1 = -1;
+                        }
+                        hasLineDeletedOrAdded = true;
+                        linesOutputF1.add("- " + line.getLine());
+                        sizeContext = 0;
+                        if (idInitialLine1 == -1) {
+                            idInitialLine1 = line.getIndLineFileV1() + 1;
+                        }
+                        idFinalLine1 = line.getIndLineFileV1() + 1;
+                    } else {
+                        if (line.getSituation() == LinesBean.Situation.UNCHANGED) {
+                            if (sizeContext == 1) {
+                                linesOutputF1.remove(0);
+                                linesOutputF2.remove(0);
+
+                                sizeContext--;
+                                idInitialLine1++;
+                                idInitialLine2++;
+                            }
+                            sizeContext++;
+                            linesOutputF1.add("  " + line.getLine());
+                            linesOutputF2.add("  " + line.getLine());
+
+                            if (idInitialLine1 == -1) {
+                                idInitialLine1 = line.getIndLineFileV1() + 1;
+                                idFinalLine1 = line.getIndLineFileV1() + 1;
+                            } else {
+                                idFinalLine1++;
+                            }
+
+                            line = (LinesBean) it.next();
+                            if (idInitialLine2 == -1) {
+                                idInitialLine2 = line.getIndLineFileV2() + 1;
+                                idFinalLine2 = line.getIndLineFileV2() + 1;
+                            } else {
+                                idFinalLine2++;
+                            }
+                        }
+                    }
+                }
+                if (it.hasNext()) {
+                    line = (LinesBean) it.next();
+                    isNext = true;
+                }
+
+            } while ((sizeContext < 1) && (it.hasNext()));
+            if ((!it.hasNext()) && (!line.getLine().isEmpty())) {
+                if (line.getSituation() == LinesBean.Situation.ADDED) {
+                    linesOutputF2.add("+ " + line.getLine());
+                    idFinalLine2++;
+                } else {
+                    if (line.getSituation() == LinesBean.Situation.REMOVED) {
+                        linesOutputF1.add("- " + line.getLine());
+                        idFinalLine1++;
+                    }
+                }
+            }
+            if (hasLineDeletedOrAdded) {
+                outputWriter.println("***************");
+                outputWriter.println(getHeader('d', idInitialLine1, idFinalLine1, idInitialLine2, idFinalLine2));
+                if (canPrint(linesOutputF1, "-")) {
+                    printOutput(linesOutputF1, outputWriter);
+                }
+                linesOutputF1.clear();
+
+                outputWriter.println(getHeader('a', idInitialLine1, idFinalLine1, idInitialLine2, idFinalLine2));
+
+                if (canPrint(linesOutputF2, "+")) {
+                    printOutput(linesOutputF2, outputWriter);
+                }
+                linesOutputF2.clear();
+
+                idInitialLine1 = -1;
+                idInitialLine2 = -1;
+                sizeContext = 0;
+                hasLineDeletedOrAdded = false;
+            }
+        }
+
     }
 
     @Override
     public String getHeader(char format, int startLine1F, int finalLine1F, int startLine2F, int finalLine2F) {
         String addHeader = null;
-        if (format == 'd') {
-            addHeader = "--- " + Integer.toString(startLine1F);
-            if (startLine1F != finalLine1F) {
-                addHeader = addHeader + "," + finalLine1F;
+        if (format == 'a') {
+            addHeader = "--- " + Integer.toString(startLine2F);
+            if (startLine2F != finalLine2F) {
+                addHeader = addHeader + "," + finalLine2F;
             }
             addHeader = addHeader + "----";
         } else {
-            addHeader = "*** " + Integer.toString(startLine2F);
-            if (startLine2F != finalLine2F) {
+            addHeader = "*** " + Integer.toString(startLine1F);
+            if (startLine1F != finalLine1F) {
                 addHeader = addHeader + "," + finalLine1F;
             }
             addHeader = addHeader + "****";
         }
         return addHeader;
+    }
+
+    private void printOutput(List listLines, PrintWriter outputWriter) {
+        for (int i = 0; i < listLines.size(); i++) {
+            System.out.println(listLines.get(i));
+            outputWriter.println(listLines.get(i));
+        }
+    }
+
+    private boolean canPrint(List listLines, String idFormat) {
+        boolean contain = false;
+        for (int i = 0; i < listLines.size(); i++) {
+            if (listLines.get(i).toString().contains(idFormat)) {
+                contain = true;
+                i=listLines.size();
+            }
+        }
+        return contain;
     }
 }
--- src/main/java/br/uff/ic/gardener/diff/UnifiedFormat.java
+++ src/main/java/br/uff/ic/gardener/diff/UnifiedFormat.java
@@ -1,34 +1,142 @@
 package br.uff.ic.gardener.diff;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class UnifiedFormat implements IFormat {
 
-    /* -------------- DETAILS DESCRIPTION OF UNIFIED FORMAT --------------
-     * --- from-file from-file-modification-time
-     * +++ to-file to-file-modification-time
-     * Unified format hunks look like this:
-     * @@ from-file-line-numbers to-file-line-numbers @@
-     * line-from-either-file
-     * line-from-either-file...
-     * If a hunk and its context contain two or more lines, its line numbers look
-     * like ‘start,count’
-     * An empty hunk is considered to end at the line that precedes the hunk.
-     * The lines common to both files begin with a space character.
-     * '+’ A line was added here to the first file.
-     * ‘-’ A line was removed here from the first file.
-     * -------------------------------------------------------------------
-     */
     @Override
     public void format(IResultDiff resultDiff) {
-        System.out.println("Unified Format");
+        int sizeContext = 0;
+        LinesBean line = null;
+        List linesOutput = new ArrayList();
+        boolean hasLineDeletedOrAdded = false;
+        int idInitialLine1 = -1, idInitialLine2 = -1, idFinalLine1 = -1, idFinalLine2 = -1;
+        boolean isNext = false;
+        boolean hasLine = false;
+
+        PrintWriter outputWriter = null;
+        try {
+            outputWriter = WriterFactory.getWriter();
+        } catch (IOException ex) {
+            Logger.getLogger(NormalFormat.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        outputWriter.println(FormatFactory.getMainHeader().split("\\!!")[0].replaceAll("\\*\\*\\*", "---"));
+        outputWriter.println(FormatFactory.getMainHeader().split("\\!!")[1].replaceAll("---", "+++"));
+        outputWriter.println();
+
+        for (Iterator it = resultDiff.getResult().iterator(); it.hasNext();) {
+            hasLine = false;
+            if (!isNext) {
+                line = (LinesBean) it.next();
+            }
+            do {
+                if (line.getSituation() == LinesBean.Situation.ADDED) {
+                    if (sizeContext > 3) {
+                        linesOutput.remove(0);
+                        sizeContext--;
+                    }
+                    hasLineDeletedOrAdded = true;
+                    linesOutput.add("+ " + line.getLine());
+                    sizeContext = 0;
+                    if (idInitialLine2 == -1) {
+                        idInitialLine2 = line.getIndLineFileV2() + 1;
+                    }
+                    idFinalLine2 = line.getIndLineFileV2() + 1;
+                } else {
+                    if (line.getSituation() == LinesBean.Situation.REMOVED) {
+                        if (sizeContext > 3) {
+                            linesOutput.remove(0);
+                            sizeContext--;
+                            idInitialLine1 = -1;
+                        }
+                        hasLineDeletedOrAdded = true;
+                        linesOutput.add("- " + line.getLine());
+                        sizeContext = 0;
+                        if (idInitialLine1 == -1) {
+                            idInitialLine1 = line.getIndLineFileV1() + 1;
+                        }
+                        idFinalLine1 = line.getIndLineFileV1() + 1;
+                    } else {
+                        if (line.getSituation() == LinesBean.Situation.UNCHANGED) {
+                            if (sizeContext == 3) {
+                                linesOutput.remove(0);
+                                sizeContext--;
+                                idInitialLine1++;
+                                idInitialLine2++;
+                            }
+                            sizeContext++;
+                            linesOutput.add("  " + line.getLine());
+                            if (idInitialLine1 == -1) {
+                                idInitialLine1 = line.getIndLineFileV1() + 1;
+                                idFinalLine1 = line.getIndLineFileV1() + 1;
+                            } else {
+                                idFinalLine1++;
+                            }
+
+                            line = (LinesBean) it.next();
+                            if (idInitialLine2 == -1) {
+                                idInitialLine2 = line.getIndLineFileV2() + 1;
+                                idFinalLine2 = line.getIndLineFileV2() + 1;
+                            } else {
+                                idFinalLine2++;
+                            }
+                        }
+                    }
+                }
+                if (it.hasNext()) {
+                    line = (LinesBean) it.next();
+                    isNext = true;
+                }
+
+            } while ((sizeContext < 3) && (it.hasNext()));
+            if ((!it.hasNext()) && (!line.getLine().isEmpty())) {
+                if (line.getSituation() == LinesBean.Situation.ADDED) {
+                    linesOutput.add("+ " + line.getLine());
+                    idFinalLine2++;
+                } else {
+                    if (line.getSituation() == LinesBean.Situation.REMOVED) {
+                        linesOutput.add("- " + line.getLine());
+                        idFinalLine1++;
+                    }
+                }
+            }
+            if (hasLineDeletedOrAdded) {
+
+                outputWriter.println(getHeader('u', idInitialLine1, idFinalLine1, idInitialLine2, idFinalLine2));
+                printOutput(linesOutput, outputWriter);
+                linesOutput.clear();
+                idInitialLine1 = -1;
+                idInitialLine2 = -1;
+                sizeContext = 0;
+                hasLineDeletedOrAdded = false;
+            }
+        }
+
     }
 
     @Override
-    public String getHeader(char formatType, int startLine1F, int finalLine1F, int startLine2F, int finalLine2F) {
+    public String getHeader(
+            char formatType, int startLine1F, int finalLine1F, int startLine2F, int finalLine2F) {
         String addHeader = "@@ -";
         //account number of lines
         addHeader = addHeader + Integer.toString(startLine1F) + "," + Integer.toString(finalLine1F - startLine1F + 1);
         addHeader = addHeader + " +";
         addHeader = addHeader + Integer.toString(startLine2F) + "," + Integer.toString(finalLine2F - startLine2F + 1);
+
         return addHeader + " @@";
+    }
+
+    private void printOutput(List listLines, PrintWriter outputWriter) {
+        for (int i = 0; i <
+                listLines.size(); i++) {
+            System.out.println(listLines.get(i));
+            outputWriter.println(listLines.get(i));
+        }
     }
 }
