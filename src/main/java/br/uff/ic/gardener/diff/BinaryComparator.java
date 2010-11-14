db1db8c69b4e3dbd6191d8f24ebe082fffd72c6a
ffloriano <ffloriano@ic.uff.br>
2010-11-14 13:47:54 -0200

    Binary diff Directory diff

---
+++ src/main/java/br/uff/ic/gardener/diff/BinaryComparator.java
@@ -0,0 +1,13 @@
+package br.uff.ic.gardener.diff;
+
+import java.io.File;
+
+class BinaryComparator implements IDiff {
+    public BinaryComparator() {
+    }
+
+    public IResultDiff diff(File fileVersionOne, File fileVersionTwo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
