af049d3862f15f14cbfe51b8521a33d46ab18a04
Alessandreia <alessandreia@gmail.com>
2010-11-16 03:57:02 -0200

    Alteracoes no Modelo de Armazenamento

--- src/main/java/br/uff/ic/gardener/database/ConfigurationBD.java
+++ src/main/java/br/uff/ic/gardener/database/ConfigurationBD.java
@@ -1,49 +1,41 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.uff.ic.gardener.database;
 
 /**
+ * Esta classe permite fornecer métodos para acesso ao servidor de
+ * banco de dados do Sistema de Controle de Versão Gardener.
+ * O Gardener é resultado de um trabalho da disciplina de Laboratório de
+ * Gerencia de Configuração.
  *
- * @author Evaldo de Oliveira
+ * Criação em 9 de Novembro de 2010.
+ *
+ * @author Alessandreia Marta de Oliveira
+ *
  */
 public class ConfigurationBD {
-
-    private int port;
+    private int    port;
     private String server;
 
-    public ConfigurationBD(int port, String server){
-
+    public ConfigurationBD(int port, String server) {
         this.setPort(port);
         this.setServer(server);
-
-
     }
 
-    public void setPort(int port){
-
+    public void setPort(int port) {
         this.port = port;
-
     }
 
-    public int getPort(){
-
+    public int getPort() {
         return this.port;
-
     }
 
-     public void setServer(String server){
-
+    public void setServer(String server) {
         this.server = server;
-
     }
 
-    public String getServer(){
-
+    public String getServer() {
         return this.server;
-
     }
-
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
